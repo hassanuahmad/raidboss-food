9781e57c397405a2c93c68c977eb0376861903ff
Hassan Ahmad <ahmad12p@uwindsor.ca>
2021-03-27 03:55:02 -0400

    Main Page done with basic buttons

---
+++ src/app.js
@@ -0,0 +1,26 @@
+// Google Signin to console info
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log("Name: " + profile.getName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+  
+  function handleSubmit() {
+    const getPostalCode = document.getElementById("postal-code").value;
+    const getDistance = document.getElementById("distance").value;
+  
+    localStorage.setItem("POSTALCODE", getPostalCode);
+    localStorage.setItem("DISTANCE", getDistance);
+  }
+  
+  // Taking the postal code and displaying it on the search.html page
+  window.addEventListener("load", () => {
+    const postalCode = localStorage.getItem("POSTALCODE");
+    const distance = localStorage.getItem("DISTANCE");
+  
+    document.getElementById("result-postalCode").innerHTML = postalCode;
+    document.getElementById("result-distance").innerHTML = distance;
+  });
+  
