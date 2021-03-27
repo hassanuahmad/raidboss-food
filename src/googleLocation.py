a57cd8810075d3b9b3a1908baa3b9fd3aad74f81
Hassan Ahmad <ahmad12p@uwindsor.ca>
2021-03-27 13:06:03 -0400

    Google Location py File by Aaron

--- src/googleLocation.py
+++ src/googleLocation.py
@@ -6,7 +6,7 @@ from urllib.parse import urlencode
 API_KEY = 'AIzaSyB9R7udTrzDh82n8EqzB9FcfcY9RMCxtK0'
 
 #gets latitude and longitude from user input of postal code ONLY WORKS IN CANADA
-postal_code = "A1B 2C3" #needs input from webpage also
+postal_code = "N9B 3J1" #needs input from webpage also
 nomi = pgeocode.Nominatim('ca')
 location = nomi.query_postal_code(postal_code)
 lat_long = location.latitude, location.longitude
@@ -18,7 +18,7 @@ places_endpoint = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json
 params_places = {
         "key": API_KEY,
         "location": f"{lat}, {long}",
-        "radius": 800,
+        "radius": 10, #needs input from the webiste
         "keyword": "curbside pickup",
         "types": "food"
         }
