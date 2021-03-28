07492b023a6d34080e183f285011e2b3c2f689e1
Hassan Ahmad <ahmad12p@uwindsor.ca>
2021-03-28 02:23:20 -0400

    Fixed the JS File and HTML

--- static/app.js
+++ static/app.js
@@ -6,20 +6,3 @@ function onSignIn(googleUser) {
   console.log("Image URL: " + profile.getImageUrl());
   console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
-
-function handleSubmit() {
-  const getPostalCode = document.getElementById("postal-code").value;
-  const getDistance = document.getElementById("distance").value;
-
-  localStorage.setItem("POSTALCODE", getPostalCode);
-  localStorage.setItem("DISTANCE", getDistance);
-}
-
-// Taking the postal code and displaying it on the search.html page
-window.addEventListener("load", () => {
-  const postalCode = localStorage.getItem("POSTALCODE");
-  const distance = localStorage.getItem("DISTANCE");
-
-  document.getElementById("result-postalCode").innerHTML = postalCode;
-  document.getElementById("result-distance").innerHTML = distance;
-});
