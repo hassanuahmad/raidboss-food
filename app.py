5b5cc6ea71d969bccd03b05e1f25a19900de508c
hassanuahmad <hassanusaidahmad@gmail.com>
2021-09-15 02:35:57 -0400

    CSS Changes to search.html

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, app, redirect, url_for, render_template, request
 import pgeocode
 import requests
-import os
+from dotenv import load_dotenv
 from urllib.parse import urlencode
 from googleplaces import GooglePlaces, types, lang
 import jinja2
+load_dotenv()
 
 app = Flask(__name__)
 env = jinja2.Environment()
@@ -22,7 +23,7 @@ def search():
     distance = request.form["distance"]
 
     # Google Places API Key
-    API_KEY = os.environ.GOOGLE_API_KEY
+    API_KEY = 'AIzaSyB9R7udTrzDh82n8EqzB9FcfcY9RMCxtK0'
     google_places = GooglePlaces(API_KEY)
 
     # gets latitude and longitude from user input of postal code ONLY WORKS IN CANADA
